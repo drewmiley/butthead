6fe853e78a68af8b724ea796becd5b3b89b9f363
Drew Miley <drewmiley92@gmail.com>
2018-06-03 20:18:41 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,23 @@
+# Butthead
+
+Attempt to create a simple js transpiler for compilation and evaluation.
+
+## Language Features
+
+Currently
+
+* File ends with _.bh_ rather than _.js_
+
+## To run
+
+### Compiler
+
+```
+something not implemented
+```
+
+### Evaluator
+
+```
+node transpile.js example.bh
+```
---
+++ transpile.js
@@ -0,0 +1,17 @@
+const { readFileSync, existsSync } = require('fs');
+
+const fileName = process.argv.pop();
+
+if (fileName.slice(-3) !== '.bh') {
+    console.error('You\'re a buttmunch!');
+    process.exit(1);
+}
+
+if (!existsSync(fileName)) {
+  console.error(`"${fileName}" does not exist.`)
+  process.exit(1);
+}
+
+const program = readFileSync(fileName, { encoding: 'utf-8' });
+
+eval(program);
