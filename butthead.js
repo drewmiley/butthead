4ed513cf92ae6d8e71202805e230870c28ad11a8
Drew Miley <drew.miley@opencastsoftware.com>
2018-06-04 10:49:24 +0100

    Adds -c, -e, -f, -o flags for running transpiler

--- butthead.js
+++ butthead.js
@@ -1,6 +1,13 @@
 const { readFileSync, writeFileSync, existsSync } = require('fs');
 
-const fileName = process.argv.pop();
+const processArguments = process.argv.slice(2);
+
+const compile = processArguments.indexOf('-c') !== -1;
+const evaluate = processArguments.indexOf('-e') !== -1;
+const fileName = processArguments.find(arg => arg.startsWith('-f=')).substring(3);
+const outputFileName = processArguments.find(arg => arg.startsWith('-o=')) ?
+    processArguments.find(arg => arg.startsWith('-o=')).substring(3) :
+    `${ fileName }.js`;
 
 if (fileName.slice(-3) !== '.bh') {
     console.error('You\'re a buttmunch!');
@@ -8,12 +15,16 @@ if (fileName.slice(-3) !== '.bh') {
 }
 
 if (!existsSync(fileName)) {
-  console.error(`"${fileName}" does not exist.`)
+  console.error(`"${fileName}" does not exist.`);
   process.exit(1);
 }
 
 const program = readFileSync(fileName, { encoding: 'utf-8' });
 
-eval(program);
+if (compile) {
+    writeFileSync(outputFileName, program);
+}
 
-writeFileSync(`${ fileName }.js`, program);
+if (evaluate) {
+    eval(program);
+}
--- readme.md
+++ readme.md
@@ -10,14 +10,15 @@ Currently
 
 ## To run
 
-### Compiler
-
 ```
-something not implemented
+node butthead.js -c -e -f=example.bh -o=output.js
 ```
 
-### Evaluator
+### Supported params
 
-```
-node transpile.js example.bh
-```
+* _-c_ (Compile)
+* _-e_ (Evaluate)
+* _-f_ (Set file name to read) __REQUIRED__
+* _-o_ (Set output file to write) For use in comp mode
+
+_Note: You can compile and evaluate at the same time if so desired_ 
